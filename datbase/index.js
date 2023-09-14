78bca2a05021e060d585c0099eb1e781bce3db90
andhac <andhac84@yahoo.com>
2023-09-14 22:08:57 +0530

    first commit

---
+++ datbase/index.js
@@ -0,0 +1,52 @@
+let books = [
+  {
+    ISBN: "1234ONE",
+    title: "The Lord of the Rings",
+    author: [1],
+    language: "en",
+    pubDate: "2000-01-01",
+    numOfPage: 100,
+    category: ["Fantasy", "Adventure", "Action", "Thriller"],
+    publication: 1,
+  },
+  {
+    ISBN: "1234TWO",
+    title: "Harry Potter and the Philosopher's Stone",
+    author: [2],
+    language: "en",
+    pubDate: "1997-01-01",
+    numOfPage: 300,
+    category: ["Fantasy", "Adventure", "Action", "Thriller"],
+    publication: 1,
+  },
+];
+let author = [
+  {
+    id: 1,
+    name: "J. R. R. Tolkien",
+    books: ["1234ONE", "1234TWO"],
+  },
+  {
+    id: 2,
+    name: "J. K. Rowling",
+    books: ["1234ONE", "1234TWO"],
+  },
+];
+let publication = [
+  {
+    id: 1,
+    name: "HarperCollins Publication",
+    books: ["1234ONE", "1234TWO"],
+  },
+  {
+    id: 2,
+    name: "Bloomsbury Publishing",
+    books: [],
+  },
+];
+
+module.exports ={
+    books,
+    author,
+    publication
+}
